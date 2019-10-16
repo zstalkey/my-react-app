25d9c0c24f85d6392ce8eeb7026720738a03594e
Zoe <Zoe.Stalkey@gmail.com>
2019-10-16 18:27:38 +0800

    amended destList to a state and add new state

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React from 'react';
 import Table from './Table';
 
 class App extends React.Component{
+  state = {
+    destList:[      {
+      destination: 'Tokyo', 
+      country: 'Japan',
+    },
+    {
+      destination: 'Vienna', 
+      country: 'Austria',
+    },
+    {
+      destination: 'British Columbia', 
+      country: 'Canada',
+    },
+    {
+      destination: 'Berlin', 
+      country: 'Germany',
+    },
+    {
+      destination: 'Tasmania', 
+      country: 'Australia',
+    },],
+  }
+  removeDestination = index => {
+    const listofDest = this.state.destList;
+    this.setState({
+      destList: listofDest.filter((dest, i) => {
+        return i !== index
+      })
+    })
+  }
   render() { 
-    const destList = [
-      {
-        destination: 'Tokyo', 
-        country: 'Japan',
-      },
-      {
-        destination: 'Vienna', 
-        country: 'Austria',
-      },
-      {
-        destination: 'British Columbia', 
-        country: 'Canada',
-      },
-      {
-        destination: 'Berlin', 
-        country: 'Germany',
-      },
-      {
-        destination: 'Tasmania', 
-        country: 'Australia',
-      },
-    ]
     return (
       <div className="App">
         <h1>Places to travel</h1>
-        <Table destinations={destList}/>
+        <Table destinations={this.state.destList} removeDestination={this.removeDestination}/>
       </div>
     )
   }
 }
+
+
 
 export default App;
