d98fe9dd0a18f42e0aa10b438df6fe8159b22f35
Zoe <Zoe.Stalkey@gmail.com>
2019-10-16 19:40:39 +0800

    Added delete button that removes.
    Note, item returns on refresh

--- src/Table.js
+++ src/Table.js
@@ -1,42 +1,47 @@
 import React from 'react';
 
 class Table extends React.Component {
-    render() {
-        const destProps = this.props.destinations
-      return (
-        <table>
-          <TableHeader />
-          <TableBody destinations = {destProps} />
-        </table>
-      )
-    }
+  render() {
+    const destProps = this.props.destinations
+    const removeDest = this.props.removeDestination
+    return (
+      <table>
+        <TableHeader />
+        <TableBody destinations={destProps} removeDestination={removeDest} />
+      </table>
+    )
   }
-  
+}
+
 
 const TableHeader = () => {
-    return (
-        <thead>
-        <tr>
-            <th>Destination</th>
-            <th>Country</th>
-        </tr>
-        </thead>
-    )
+  return (
+    <thead>
+      <tr>
+        <th>Destination</th>
+        <th>Country</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+  )
 }
 
 const TableBody = (props) => {
-    const rows = props.destinations.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.destination}</td>
-          <td>{row.country}</td>
-        </tr>
-      )
-    })
+  const rows = props.destinations.map((row, index) => {
     return (
-      <tbody>{ rows }</tbody>
+      <tr key={index}>
+        <td>{row.destination}</td>
+        <td>{row.country}</td>
+        <td><button onClick={() => {
+          props.removeDestination(index);
+        }}>Delete</button></td>
+      </tr>
     )
-   }
+  })
+  return (
+    <tbody>{rows}</tbody>
+  )
+}
 
 
 export default Table
